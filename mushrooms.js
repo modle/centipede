10bf7c9c2584a6d2ed3be0599036ae5d72383bf8
Matthew Odle <matt.odle@tdstelecom.com>
2017-04-05 22:25:27 -0500

    mushrooms now generate on a grid based on canvas dimensions

--- mushrooms.js
+++ mushrooms.js
@@ -12,15 +12,34 @@ function manageMushrooms() {
 }
 
 function spawnMushrooms(amount) {
+  getEligiblePositions();
   while (mushrooms.length < amount) {
-    x = Math.floor(Math.random() * (canvasWidth + 1));
-    y = Math.floor(Math.random() * (canvasHeight * .8 + 1));
-    mushrooms.push(new component(15, 15, "Plum", x, y));
+    x = xEligiblePositions[Math.floor(Math.random()*xEligiblePositions.length)];
+    y = yEligiblePositions[Math.floor(Math.random()*yEligiblePositions.length)];
+    mushrooms.push(new component(canvasWidth / 25, canvasHeight / 22, "Plum", x, y));
   }
 }
 
 function updateMushrooms() {
   for (i = 0; i < mushrooms.length; i += 1) {
     mushrooms[i].update();
+  }
+}
+
+xEligiblePositions = []
+yEligiblePositions = []
+
+function getEligiblePositions() {
+  xSpacing = canvasWidth / 25;
+  ySpacing = canvasHeight / 22;
+  x = 0;
+  y = 0;
+  while (x < canvasWidth) {
+    xEligiblePositions.push(x);
+    x += xSpacing;
+  }
+  while (y < canvasHeight * .75) {
+    yEligiblePositions.push(y);
+    y += ySpacing;
   }
 }
